ef12a8c590f8524d52bdfcd07838fe2787cfc429
Rares-Andrei Dancau <rares-andrei.dancau@student.tuiasi.ro>
2023-05-08 22:27:37 +0300

    Laborator 11 tema 3

--- app.js
+++ app.js
@@ -19,10 +19,17 @@ app.use(bodyParser.urlencoded({
     extended: true
 }));
 app.use(cookieParser());
+const session = require('express-session');
+app.use(session({
+  secret: 'secret-key',
+  resave: true,
+  saveUninitialized: false
+}));
 // proprietățile obiectului Request - req - https://expressjs.com/en/api.html#req
 // proprietățile obiectului Response - res - https://expressjs.com/en/api.html#res
 app.get('/', (req, res) => {
     const utilizator = req.cookies.utilizator;
+    res.clearCookie('mesajEroare');
     res.render('index', {
         utilizator: utilizator,
         layout: 'layout'
@@ -31,11 +38,13 @@ app.get('/', (req, res) => {
 app.get('/favicon.ico', (req, res) => res.send('Hello World'));
 // la accesarea din browser adresei http://localhost:6789/chestionar se va apela funcția specificată
 app.get('/chestionar', (req, res) => {
+  const utilizator = req.cookies.utilizator;
     // în fișierul views/chestionar.ejs este accesibilă variabila 'intrebari' care conține vectorul de întrebări
     fs.readFile('./intrebari.json', (err, data) => {
         if (err) throw err;
         var listaIntrebari = JSON.parse(data);
         res.render('chestionar', {
+          utilizator: utilizator,
             intrebari: listaIntrebari,
             layout: 'layout'
         });
@@ -43,6 +52,7 @@ app.get('/chestionar', (req, res) => {
 });
 
 app.post('/rezultat-chestionar', (req, res) => {
+  const utilizator = req.cookies.utilizator;
     let numarRaspunsuriCorecte = 0;
     console.log(req.body);
     fs.readFile('./intrebari.json', (err, data) => {
@@ -57,13 +67,16 @@ app.post('/rezultat-chestionar', (req, res) => {
         }
         const rezultat = `Ai răspuns corect la ${numarRaspunsuriCorecte} din 7 întrebări.`;
         res.render('rezultat-chestionar', {
+          utilizator: utilizator,
             rezultatC: rezultat,
             layout: 'layout'
         });
     });
 });
 app.get('/autentificare', function(req, res) {
+  const utilizator = req.cookies.utilizator;
     res.render('autentificare', {
+      utilizator: utilizator,
         mesajEroare: req.cookies.mesajEroare,
         layout: 'layout'
     });
@@ -71,14 +84,37 @@ app.get('/autentificare', function(req, res) {
 app.post('/verificare-autentificare', function(req, res) {
     const utilizator = req.body.utilizator;
     const parola = req.body.parola;
+    const utilizatori = JSON.parse(fs.readFileSync('./utilizatori.json'));
 
-    if (utilizator === 'utilizator' && parola === 'parola') {
+    // Parcurgem lista de utilizatori pentru a căuta o combinație validă de utilizator și parolă
+    let utilizatorValid = false;
+    for (const u of utilizatori) {
+      if (u.utilizator === utilizator && u.parola === parola) {
+        utilizatorValid = true;
+        // Setăm cookie-ul pentru utilizator
+        req.session.utilizator = utilizator;
+        req.session.nume=u.nume;
+        req.session.prenume=u.prenume;
+        req.session.varsta=u.varsta;
+        break;
+      }
+    }
+    if (utilizatorValid) {
         res.cookie('utilizator', utilizator);
+        res.clearCookie('mesajEroare');
         res.redirect('/');
+        console.log(req.session.nume)
     } else {
         res.cookie('mesajEroare', 'Nume de utilizator sau parolă incorecte. Vă rugăm să încercați din nou!');
         res.redirect('/autentificare');
     }
+});
+app.post('/logout', function(req, res) {
+  req.session.destroy(function(err) {
+    res.clearCookie('utilizator');
+    res.clearCookie('mesajEroare');
+    res.redirect('/');
+  });
 });
 
 app.listen(port, () => console.log(`Serverul rulează la adresa http://localhost:` + port));
